02fd7e2c720d350b5f43ea91c8d20ff38a8e5444
Richo Healey <richo@psych0tik.net>
2015-06-14 18:10:37 -0700

    Add the blocks module

---
+++ src/blocks/mod.rs
@@ -0,0 +1,5 @@
+pub mod section_header;
+pub mod enhanced_packet;
+
+pub use self::section_header::SectionHeader;
+pub use self::enhanced_packet::EnhancedPacket;
---
+++ src/options.rs
@@ -0,0 +1,3 @@
+// FIXME(richo) Flesh this out properly with it's own discrete parser.
+struct Options;
+
